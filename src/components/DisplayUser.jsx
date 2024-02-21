972cac585c6f7af29bb2fc2efdc53643fe392228
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-02-21 22:08:03 +0200

    Writing Markup with JSX

---
+++ src/components/DisplayUser.jsx
@@ -0,0 +1,5 @@
+export default function DisplayUser() {
+    return (
+        <div>DisplayUser</div>
+    )
+}
---
+++ src/views/HomePage.jsx
@@ -0,0 +1,10 @@
+import {Employee} from "../components/Employee";
+
+export default function HomePage() {
+    return (
+        <div>
+            <h2>HomePage</h2>
+            <Employee />
+        </div>
+    )
+}
