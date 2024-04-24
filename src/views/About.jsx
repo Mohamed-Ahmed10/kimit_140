030732c461172c7a909f4388cde539ec508dca7a
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2024-04-24 18:06:49 +0200

    Hooks

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,15 +1,35 @@
 // import { useContext } from "react";
 // import { ProductsContext } from "../App";
 
+import { useRef, useEffect } from "react";
+
 export default function About() {
 
-    // let products = useContext(ProductsContext)
 
-    // console.log(products)
+    var inputRef = useRef();
+
+
+    var parent = useRef();
+
+
+    useEffect(() => {
+        inputRef.current.focus()
+        console.log(parent.current.children)
+    }, [])
+
     return (
         <div>
             <h2>I am about</h2>
-            {/* {products.numOfProducts} */}
+            <div ref={parent} id="hello">
+                <div className="test">test</div>
+                <div className="test">test</div>
+                <div className="test">test</div>
+                <div className="test">test</div>
+                <div className="test">test</div>
+                <div className="test">test</div>
+            </div>
+
+            <input type="text" ref={inputRef} />
         </div>
     );
 }
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,17 +1,30 @@
-import { useState } from "react";
-import InputContact from "../components/InputContact";
-import { Button } from "react-bootstrap"
-export default function Contact() {
-    let [show, setShow] = useState(true)
+import { useMemo, useState } from "react"
 
-    let handleShow = () => {
-        setShow(!show)
-    }
+export default function Contact() {
+    let [firstName, setFirstName] = useState("")
+    let [lastName, setLastName] = useState("")
+    let [age, setAge] = useState("")
+
+    let randomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
+    
+    let fullName = useMemo(() => (
+        <div style={{ color: randomColor() }}>your full name is : {firstName + " " + lastName} </div>
+    ), [firstName, lastName])
+
     return (
         <div>
             <h2>Contact</h2>
-            <Button variant="primary" onClick={handleShow}>{show ? "Hide" : "Show"}</Button>
-            {show && <InputContact />}
+
+            <input type="text" onChange={ev => setFirstName(ev.target.value)} />
+            <input type="text" onChange={ev => setLastName(ev.target.value)} />
+            <input type="number" onChange={ev => setAge(ev.target.value)} />
+            <hr />
+
+            <div>your first name is : {firstName} </div>
+            {fullName}
+            <div>your last name is : {lastName} </div>
+
+            <div>your age is : {age} </div>
         </div>
     )
 }
